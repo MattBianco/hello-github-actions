93ee8a199e0785c9b42b3acec95132276d6ac749
MattBianco <35449068+MattBianco@users.noreply.github.com>
2022-02-17 08:54:46 +0100

    change wording

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -l
 
-sh -c "echo 'Hello, world! $INPUT_MY_NAME has the value "'$INPUT_MY_NAME'"'"
+sh -c "echo 'Hello, world! $INPUT_MY_NAME is passed by environment variable "'$INPUT_MY_NAME'"'"
